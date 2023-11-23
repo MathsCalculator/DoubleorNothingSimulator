9dbfdd30687b6fcb71a7cacd24a01355a3f23ca1
mathscalculator <arnomathei@email.com>
2023-11-23 17:32:30 +0100

    Fixed inaccurate ETA results

--- BlueSlots Double or Nothing Simulator.py
+++ BlueSlots Double or Nothing Simulator.py
@@ -1,11 +1,15 @@
 # Copyright (c) [2023], [mathscalculator]
 
+# Importing necessary modules
 import random, os, time, math, winsound, subprocess
+
+# Initializing global variables
 totalAttempts = 0
 totalSimulationsAmount = None
 linesplit = "------------------------------------------------------------"
 logfile = None
 
+# Custom exceptions for user interaction
 class UserAskedHelp(Exception):
     def __init__(self, message = ""):
         super().__init__(message)
@@ -14,7 +18,9 @@ class UserResetInputs(Exception):
     def __init__(self, message = ""):
         super().__init__(message)
 
+# Function to convert seconds into human-readable time
 def GetTotalTimeTaken(seconds):
+    # List of time units and their corresponding seconds
     units = [
         ('year', 86400 * 7 * 52.178),
         ('month', 86400 * 7 * 4.348),
@@ -25,29 +31,33 @@ def GetTotalTimeTaken(seconds):
         ('second', 1)
     ]
     result = []
+    # Iterate over units to build the time representation
     for unitName, unitSeconds in units:
         if seconds >= unitSeconds:
-            unitCount = math.floor(seconds / unitSeconds)
+            unitCount = round(seconds / unitSeconds)
             seconds %= unitSeconds
             if unitCount > 1:
                 result.append(f"{unitCount} {unitName}s")
             else:
                 result.append(f"{unitCount} {unitName}")
+    # Format the result
     if result:
         return ', '.join(result)
     else:
         return "0 seconds"
 
+# Function to print ETA during simulations
 def PrintSimulationsProcessETA(i):
     global days, hours, minutes, seconds, maxTime
     seconds = i
     if seconds is None:
         return
+    if maxTime < seconds:
+        maxTime = seconds + 1
     days = seconds / 86400
     hours = (seconds % 86400) / 3600
     minutes = ((seconds % 86400) % 3600) / 60
-    if maxTime < seconds:
-        maxTime = seconds
+    # Display ETA based on the duration of the simulation
     if seconds > (86400 * 7 * 52.178):
         years = seconds / (86400 * 7 * 52.178)
         print(f'{format(grandTotalAttempts, ",d")} calculations done | ETA: {math.ceil(round(years, 2) * 10) / 10} year(s) (you will need a quantum computer){" " * 5}', end="\r")
@@ -60,22 +70,23 @@ def PrintSimulationsProcessETA(i):
     elif seconds > (86400 * 2):
         print(f'{format(grandTotalAttempts, ",d")} calculations done | ETA: {math.ceil(round(days, 2) * 10) / 10} days{" " * 50}', end="\r")
     elif seconds > 86400:
-        print(f'{format(grandTotalAttempts, ",d")} calculations done | ETA: {math.floor(days)} day, {math.ceil(round(hours, 2) * 10) / 10} hr(s){" " * 50}', end="\r")
+        print(f'{format(grandTotalAttempts, ",d")} calculations done | ETA: {math.floor(days)} day, {math.ceil(hours * 10) / 10} hr(s){" " * 50}', end="\r")
     elif seconds > 3600:
         print(f'{format(grandTotalAttempts, ",d")} calculations done | ETA: {math.floor(hours)} hr(s), {math.ceil(minutes)} min(s){" " * 50}', end="\r")
     elif seconds > 60:
-        print(f'{format(grandTotalAttempts, ",d")} calculations done | ETA: {math.ceil(round(minutes, 2) * 10) / 10} min(s){" " * 50}', end="\r")
+        print(f'{format(grandTotalAttempts, ",d")} calculations done | ETA: {math.ceil(minutes * 10) / 10} min(s){" " * 50}', end="\r")
     else:
-        print(f'{format(grandTotalAttempts, ",d")} calculations done | ETA: {math.ceil(round(seconds, 0))} second(s){" " * 50}', end="\r")
+        print(f'{format(grandTotalAttempts, ",d")} calculations done | ETA: {round(seconds)} second(s){" " * 50}', end="\r")
 
+# Function to calculate ETA based on the overall simulation progress
 def GetSimulationsProcessETA(override):
-    global attemptsInSec, initialTime, timestampUnknownETA
+    global attemptsInSec, initialTime
     if override is True:
         if totalSimulationsAmount < grandTotalAttempts:
             print(f'{format(grandTotalAttempts, ",d")} calculations done | ETA: Now{" " * 50}', end="\r")
         else:
             PrintSimulationsProcessETA(seconds)
-    if time.time() - initialTime > 1 and time.time() - initialTime < 5:
+    if (time.time() - initialTime) > 1 and initialTime != 0:
         if totalSimulationsAmount < grandTotalAttempts:
             print(f'{format(grandTotalAttempts, ",d")} calculations done | ETA: Now{" " * 50}', end="\r")
             initialTime = 0
@@ -87,14 +98,17 @@ def GetSimulationsProcessETA(override):
     elif initialTime == 0:
         initialTime = time.time()
 
+# Function to calculate the required number of simulations
 def GetRequiredSimulationsAmount(chancePerSim):
     requiredSimulationsAmount = GetTotalSimulationsAmount() * (100 / chancePerSim)
     return math.ceil(requiredSimulationsAmount)
 
+# Function to calculate the total number of simulations
 def GetTotalSimulationsAmount():
     totalSims = simulationsAmount * calculationsAmount
     return totalSims
 
+# Function to get user input for simulation parameters
 def GetUserInput():
     global simulationsAmount, multiplier, percentage, price, calculationsAmount, jackpot, totalSimulationsAmount, decimalPlaces, targetPercentage
     correctInputs = 0
@@ -202,14 +216,6 @@ def GetUserInput():
             price = int(price)
             if price < 1:
                 raise ValueError(f"Value {price} of type {type(price)} is not larger than 0")
-            if doLog:
-                logs = open(logfile, "a")
-                logs.write(f'Number: {str(multiplier)}\nJackpot amount: {str(jackpot)}\nSims per calculation: {str(simulationsAmount)}\nAmount of calculations: {str(calculationsAmount)}\nWin rate percentage: {str(percentage)}\nPrice per use: {str(price)}\n\n')
-                logs.close()
-            chancePerSim = (((percentage / 100) ** multiplier) * 100)
-            totalSimulationsAmount = GetRequiredSimulationsAmount(chancePerSim)
-            print("Winning chance per simulation: ", chancePerSim, "%")
-            print("Average amount of simulations: ", format(totalSimulationsAmount, ",d"))
             break
         except TypeError as e:
             input(f"Error: {e}")
@@ -222,6 +228,12 @@ def GetUserInput():
         except UserResetInputs:
             correctInputs = 0
             os.system('cls')
+
+    # Declare some global variables for other functions
+    chancePerSim = (((percentage / 100) ** multiplier) * 100)
+    totalSimulationsAmount = GetRequiredSimulationsAmount(chancePerSim)
+    print("Winning chance per simulation: ", chancePerSim, "%")
+    print("Average amount of simulations: ", format(totalSimulationsAmount, ",d"))
     if '.' in str(percentage):
         decimalPosition = str(percentage).index('.')
         decimalPlaces = (len(str(percentage)) - decimalPosition - 1) + 2
@@ -230,6 +242,7 @@ def GetUserInput():
     targetPercentage = int(str(percentage).replace(',', '').replace('.', ''))
     print("------------------------------------------------------------")
 
+# Function to start the simulation process
 def StartSimulating(numb):
     global totalAttempts, grandTotal, finishedCalculations, grandTotalAttempts, attemptsInSec, calcFeedbacks
     print(f"Calculating #{numb}", " " * 50)
@@ -237,42 +250,36 @@ def StartSimulating(numb):
         GetSimulationsProcessETA(True)
     finishedCalculations += 1
     attempts = 0
-    addup = []
     totalWon = 0
     totalLost = 0
     totalAttempts = 0
     for i in range(0, simulationsAmount):
         attempts = 0
         while True:
-            isok = True
-            totalAttempts = totalAttempts + 1
-            attempts = attempts + 1
+            isOk = True
+            totalAttempts += 1
+            attempts += 1
             grandTotalAttempts += 1
             attemptsInSec += 1
             GetSimulationsProcessETA(False)
             for i in range(0, multiplier):
-                if random.randint(0, 10 ** decimalPlaces) <= targetPercentage:
-                    addup.append(1)
-                else:
-                    addup.append(0)
-            for i in addup:
-                if i == 0:
-                    isok = False
-            addup = []
-            if isok is True:
+                if not random.randint(0, 10 ** decimalPlaces) <= targetPercentage:
+                    isOk= False
+            if isOk is True:
                 break
         if multiplier == 9:
             totalWon = totalWon + jackpot
         else:
-            totalWon = totalWon + price * (2 ** multiplier)
-        totalLost = totalLost + price * attempts
+            totalWon += price * (2 ** multiplier)
+        totalLost += price * attempts
     print(f'{format(totalWon - totalLost, ",d")}$ with {format(totalAttempts, ",d")} total attempts{" " * 50}')
     # Keep output in case of logging
     tempresult = f'{format(totalWon - totalLost, ",d")} with {format(totalAttempts, ",d")} total attempts'
     calcFeedbacks.append(tempresult)
 
-    grandTotal = grandTotal + totalWon - totalLost
+    grandTotal += totalWon - totalLost
 
+# Function to get a y/n input
 def AskYesNo(question):
     while True:
         try:
@@ -285,21 +292,23 @@ def AskYesNo(question):
         except ValueError as e:
             print(f"Error: {e}")
 
+# Function to ask the user about logging results
 def AskToLog():
-    global doLog, logfile
-    doLog = False
+    global logfile
     if AskYesNo("Would you like to log the results? (y/n): "):
-        doLog = True
-    if doLog:
         if not os.path.exists("Logs"):
             os.makedirs("Logs")
         i = len(os.listdir("Logs")) + 1
         open('Logs\log_' + str(i) + '.txt', "w")
         logfile = 'Logs\log_' + str(i) + '.txt'
         print(f"Created Logs\log_{i}")
+        return True
+    return False
 
+# Function to reset variables for a new simulation
 def ResetVariables():
-    global finishedCalculations, grandTotalAttempts, initialTime, days, hours, minutes, seconds, attemptsInSec, maxTime, grandTotal, doLog, calcFeedbacks, timestampUnknownETA
+    global finishedCalculations, grandTotalAttempts, initialTime, days, hours, minutes, seconds, attemptsInSec, maxTime, grandTotal, calcFeedbacks
+    # Also declares the variables (some of them at least)
     finishedCalculations = 0
     grandTotalAttempts = 0
     initialTime = 0
@@ -308,10 +317,9 @@ def ResetVariables():
     attemptsInSec = 0
     maxTime = 0
     grandTotal = 0
-    doLog = False
     calcFeedbacks = []
-    timestampUnknownETA = 0
 
+# Main function to start the simulation program
 def StartProgram():
     ResetVariables()
     GetUserInput()
@@ -324,30 +332,29 @@ def StartProgram():
     if grandTotal > 0:
         print("Total profit:", format(grandTotal, ",d"), " " * 50)
         print(f"Average profit per simulation: {format(round(abs(grandTotal / GetTotalSimulationsAmount())), ',.0f')}")
-        if doLog:
-            tempresult = f'\nTotal profit: {format(grandTotal, ",.2f")}\nAverage profit per simulation: {format(round(abs(grandTotal / GetTotalSimulationsAmount())), ",.0f")}\nAverage attempts per simulation: {format(math.ceil(grandTotalAttempts / (simulationsAmount * calculationsAmount)), ",.0f")}\n{linesplit}\nTotal calculations done: {format(abs(grandTotalAttempts), ",d")}\n{linesplit}\nTotal time taken: {GetTotalTimeTaken(timeFinished - timeStarted)}'
     if grandTotal < 0:
         print("Total loss:", format(abs(grandTotal), ",d"), " " * 50)
         print(f"Average loss per simulation: {format(round(abs(grandTotal / GetTotalSimulationsAmount())), ',.0f')}")
-        if doLog:
-            tempresult = f'\nTotal loss: {format(abs(grandTotal), ",.2f")}\nAverage loss per simulation: {format(round(abs(grandTotal / GetTotalSimulationsAmount())), ",.0f")}\nAverage attempts per simulation: {format(math.ceil(grandTotalAttempts / (simulationsAmount * calculationsAmount)), ",.0f")}\n{linesplit}\nTotal calculations done: {format(abs(grandTotalAttempts), ",d")}\n{linesplit}\nTotal time taken: {GetTotalTimeTaken(timeFinished - timeStarted)}'
     print(f"Average attempts per simulation: {format(math.ceil(grandTotalAttempts / (simulationsAmount * calculationsAmount)), ',.0f')}")
     print(linesplit)
     print(f"Total calculations done: {format(abs(grandTotalAttempts), ',d')}")
     print(f"Estimated calculations: {format(abs(totalSimulationsAmount), ',d')}")
     print(linesplit)
-    print(f"Total time taken: {GetTotalTimeTaken(timeFinished - timeStarted)}")
-    print(f"Estimated time: {GetTotalTimeTaken(maxTime)}")
+    totalTimeTaken = GetTotalTimeTaken(timeFinished - timeStarted)
+    estimatedTimeTaken = GetTotalTimeTaken(maxTime)
+    print(f"Total time taken: {totalTimeTaken}")
+    print(f"Estimated time: {estimatedTimeTaken}")
     print(linesplit)
-    if grandTotalAttempts == totalSimulationsAmount:
-        print(f"ETA was 100% accurate")
-    elif grandTotalAttempts < totalSimulationsAmount:
-        print(f"ETA was {math.floor((grandTotalAttempts / totalSimulationsAmount) * 10000) / 100}% accurate")
+    print(timeFinished - timeStarted)
+    print(maxTime)
+    if totalTimeTaken == estimatedTimeTaken:
+        print(f"ETA was ~100% accurate")
+    elif round(timeFinished - timeStarted) < round(maxTime):
+        print(f"ETA was {math.floor((round(timeFinished - timeStarted) / round(maxTime)) * 10000) / 100}% accurate")
     else:
-        print(f"ETA was {math.floor(maxTime / (timeFinished - timeStarted)) * 10000 / 100}% accurate")
+        print(f"ETA was {math.floor((round(maxTime) / round(timeFinished - timeStarted)) * 10000) / 100}% accurate")
     # Ask user to log and log if approved
-    AskToLog()
-    if doLog:
+    if AskToLog():
         logs = open(logfile, "a")
         # Log the user parameters input
         logs.write(f'Number: {str(multiplier)}\nJackpot amount: {str(jackpot)}\nSims per calculation: {str(simulationsAmount)}\nAmount of calculations: {str(calculationsAmount)}\nWin rate percentage: {str(percentage)}\nPrice per use: {str(price)}\n\n')
@@ -369,12 +376,17 @@ def StartProgram():
     os.system('cls')
     StartProgram()
 
+# Function to print program information to the user
 def PrintInfo():
     os.system('cls')
-    print("Welcome to the BlueSlots Double or Nothing simulator\nThis program simulates a player continuously using the Double or Nothing slot machine\nJust choose your settings and conditions and the program will start the simulations\nYou can reset your input by typing 'RESET' when asked any parameter\nIf you need an explanation with a condition, type 'HELP'\nYou can log your results when the program is finished if necessary\nFor feedback or bug reports, contact me on discord: mathscalculator\n(Program is case insensitive)")
+    print("Welcome to the BlueSlots Double or Nothing simulator\nThis program simulates a player continuously using the Double or Nothing slot machine\n"
+          "Just choose your settings and conditions and the program will start the simulations\nYou can reset your input by typing 'RESET' when asked any parameter\n"
+          "If you need an explanation with a condition, type 'HELP'\nYou can log your results when the program is finished if necessary\n"
+          "For feedback or bug reports, contact me on discord: mathscalculator\n(ETA rounds the numbers, might be slighty inaccurate)")
     input("\nPress enter to continue")
     os.system('cls')
 
+# Entry point of the program
 if __name__ == "__main__":
     PrintInfo()
     StartProgram()
